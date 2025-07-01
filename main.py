79c7adcfaa597e785831f7d1db746586c4756a52
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2025-07-01 14:09:33 -0400

    Initialize pygame and set up the main display window

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import sys
+import pygame
 
+# initialize pygame
+pygame.int()
+
+# screen size
+WIDTH, HEIGHT = 900,700
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space ")
